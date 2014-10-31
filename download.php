c37afd5adc149d23d88b768979bea488f7e30416
Anthony Kuske <anthonykuske@gmail.com>
2014-10-31 13:07:18 +0000

    Fix downloading. Specify version as parameter

--- download.php
+++ download.php
@@ -1,22 +1,38 @@
 <?php
 
-$angularVersion = '1.2.23';
+if (empty($argv[1])) {
+    die("\nPlease specify an Angular version as the first parameter");
+} else {
+    $angularVersion = $argv[1];
+}
+
 $outputDir = __DIR__ . '/i18n/';
 $baseUrl = "https://code.angularjs.org/{$angularVersion}/i18n/";
 
+function emptyDir($dir) {
+    $files = glob($dir . '/*');
+    foreach ($files as $file) {
+      if (is_file($file)) {
+        unlink($file);
+      }
+    }
+}
+
 function downloadLocale($name) {
 	global $baseUrl, $outputDir;
-
-	$fileUrl = $baseUrl . $outputDir;
-	 
+	$fileUrl = $baseUrl . $name;
 	file_put_contents($outputDir . $name, file_get_contents($fileUrl));
 }
 
+// Clear output directory
+emptyDir($outputDir);
+
+// Fetch the index page
 $doc = new DOMDocument();
 $doc->loadHTMLFile($baseUrl);
 
+// Download all angular_locale_* files listed
 $nodes = $doc->getElementsByTagName('a');
-
 foreach ($nodes as $node) {
 	$thisUrl = $node->nodeValue;
 
